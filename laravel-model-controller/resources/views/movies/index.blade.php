845cc7236a9a54ce57339751b96a2fd4cc2d8bfc
alessandroordetti <alessandro.ordetti@gmail.com>
2022-05-13 09:41:18 +0200

    Adding cards

--- laravel-model-controller/resources/views/movies/index.blade.php
+++ laravel-model-controller/resources/views/movies/index.blade.php
@@ -7,12 +7,14 @@
         <div class="container">
             <div class="row">
                 @foreach ($movies as $movie)
-                    <div class="col-3 mb-5">
-                        <h3>{{$movie->title}}</h3>
-                        <h3>{{$movie->original_title}}</h3>
-                        <h4>{{$movie->date}}</h4>
-                        <h5>{{$movie->vote}}</h5>
-                    </div>
+                        <div class="card col-3 mb-5">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$movie->title}}</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate laudantium modi hic tempora tenetur quam! Quaerat et, consequatur iste rem facere expedita, ducimus earum velit dignissimos consectetur enim adipisci delectus.</p>
+                                <a href="#" class="btn btn-primary">{{$movie->original_title}}</a>
+                            </div>
+                        </div>
                 @endforeach 
             </div>
         </div>
